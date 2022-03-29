cf78a724dc2e69ff2dcafea3385fc19026a56cfc
Tiago <TVTH059@gmail.com>
2022-03-29 21:43:46 +0200

    On to the next thing
    lessgoo

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health;
+    public float value;
+
+    void Update()
+    {
+        
+    }
+}
